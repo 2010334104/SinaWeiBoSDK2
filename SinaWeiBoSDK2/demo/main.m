9b84578f2cc5cde707e63fa72d28f63a44b488e8
jaunce <jauncezhang@gmail.com>
2012-07-04 16:59:17 +0800

    first commit

---
+++ SinaWeiBoSDK2/demo/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  WeiBoSDK2
+//
+//  Created by Wang Buping on 11-12-7.
+//  Copyright (c) 2011 Sina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SinaWeiBoSDKDemoAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, nil);
+    [pool release];
+    return retVal;
+}
